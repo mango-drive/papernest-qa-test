3b90a39f855313d31d0b9af57f2ff210b5e1622c
Seb <sebastien.corrigan@gmail.com>
2022-11-25 19:19:16 +0100

    test: votre address and vos informations

--- cypress/e2e/newspaper_flow.cy.js
+++ cypress/e2e/newspaper_flow.cy.js
@@ -1,24 +1,36 @@
 import { randomAlphaNumeric } from "../support/utils";
 
-
-
 // Urls and pathnames
 const providersUrl =
   "https://app.papernest.com/onboarding?anonymous&anonymousId=test&id_text=1&destination=newspaper";
 const referencePathname = "/mon-compte/presse/2";
 const addressPathname = "/mon-compte/presse/3";
 const userInfoPathname = "/mon-compte/presse/4";
+const datePathname = "/mon-compte/presse/5";
 
 // Selectors
 const providersSel = '*[id^="newspaper-address_change.provider-"]';
 const referenceSel = '*[id^="newspaper-address_change.reference"]';
-const buttonNextSel = '#button_next';
-const housingAddressSel = '*[id^="housing.address"]'
-const addressDropdownSel = '*[class^="dropdown-suggestions ng-star-inserted"]'
+const buttonNextSel = "#button_next";
+const housingAddressSel = '*[id^="housing.address"]';
+const addressDropdownSel = '*[class^="dropdown-suggestions ng-star-inserted"]';
 
-// User variables for the purpose of confirmation testing
-const userAddress = '157 Boulevard Macdonald 75019 Paris';
+const userInfoSelectors = {
+  firstName: '*[id^="user.first_name"]',
+  lastName: '*[id^="user.last_name"]',
+  email: '*[id^="user.email"]',
+  phoneNumber: '*[id^="user.phone_number"]',
+};
+
+// User variables for the purpose of testing the confirmation page
 let selectedProviderName;
+const user = {
+  firstName: "Sebastien",
+  lastName: "Corrigan",
+  email: randomAlphaNumeric(8) + ".test@papernest.com",
+  address: "157 Boulevard Macdonald 75019 Paris",
+  phoneNumber: "0600000000",
+};
 
 describe(
   "Flow: Newspaper Address Change",
@@ -29,10 +41,10 @@ describe(
     });
 
     afterEach(function () {
-      if (this.currentTest.state === 'failed') {
-        Cypress.runner.stop()
+      if (this.currentTest.state === "failed") {
+        Cypress.runner.stop();
       }
-    })
+    });
 
     describe("Newspaper page", () => {
       it("displays newspaper providers", () => {
@@ -43,10 +55,9 @@ describe(
 
       it("redirects to Numero d'abonné page on provider click", () => {
         // Select a random newspaper provider
-        const selectedProvider = cy.get(providersSel)
-          .then(($providers) => {
-            return cy.get(Cypress._.sample($providers));
-          });
+        const selectedProvider = cy.get(providersSel).then(($providers) => {
+          return cy.get(Cypress._.sample($providers));
+        });
 
         // store the provider name for later tests
         selectedProvider.find('*[class^="text line"]').then((line) => {
@@ -60,28 +71,64 @@ describe(
 
     describe("Numero d'abonné page", () => {
       it("accepts a subscriber number and redirects to address page", () => {
-        cy.get(buttonNextSel).should('be.visible').and('have.class', 'disabled')
+        cy.get(buttonNextSel)
+          .should("be.visible")
+          .and("have.class", "disabled");
 
         cy.get(referenceSel).type(randomAlphaNumeric(5));
 
-        cy.get(buttonNextSel).should('be.visible').and('not.have.class', 'disabled').click()
-        cy.location("pathname").should("eq", addressPathname)
+        cy.get(buttonNextSel)
+          .should("be.visible")
+          .and("not.have.class", "disabled")
+          .click();
+        cy.location("pathname").should("eq", addressPathname);
       });
     });
 
-    describe("Address page", () => {
-      it("accepts an address", () => {
-        cy.get(buttonNextSel).should('be.visible').and('have.class', 'disabled')
+    describe("Votre addresse page", () => {
+      it("accepts an address and redirects to Vos informations page", () => {
+        cy.get(buttonNextSel)
+          .should("be.visible")
+          .and("have.class", "disabled");
 
-        cy.get(housingAddressSel).should('not.have.class', 'checked').type(userAddress)
+        cy.get(housingAddressSel)
+          .should("not.have.class", "checked")
+          .type(user.address);
 
-        cy.get(addressDropdownSel).contains(userAddress).click()
+        cy.get(addressDropdownSel).contains(user.address).click();
 
-        cy.get(housingAddressSel).should('have.class', 'checked')
+        cy.get(housingAddressSel).should("have.class", "checked");
 
-        cy.get(buttonNextSel).should('be.visible').and('not.have.class', 'disabled').click()
-        cy.location("pathname").should("eq", userInfoPathname)
-      })
-    })
+        cy.get(buttonNextSel)
+          .should("be.visible")
+          .and("not.have.class", "disabled")
+          .click();
+        cy.location("pathname").should("eq", userInfoPathname);
+      });
+    });
+
+    describe("Vos informations page", () => {
+      it("accepts user information and redirects to Date page", () => {
+        cy.location("pathname").should("eq", userInfoPathname);
+
+        cy.get(buttonNextSel)
+          .should("be.visible")
+          .and("have.class", "disabled");
+
+        for (const [key, selector] of Object.entries(userInfoSelectors)) {
+          cy.get(selector)
+            .and("not.have.class", "checked")
+            .type(user[key])
+            .should("have.class", "checked");
+        }
+
+        cy.get(buttonNextSel)
+          .should("be.visible")
+          .and("not.have.class", "disabled")
+          .click();
+
+        cy.location("pathname").should("eq", datePathname)
+      });
+    });
   }
 );
