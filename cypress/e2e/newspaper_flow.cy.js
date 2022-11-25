68a3a7e213877dddcad908e14f9da63dccab30bb
Seb <sebastien.corrigan@gmail.com>
2022-11-25 12:50:23 +0100

    test: numero d'abonne page

--- cypress/e2e/newspaper_flow.cy.js
+++ cypress/e2e/newspaper_flow.cy.js
@@ -1,24 +1,33 @@
+import {randomAlphaNumeric, randomIntFromInterval} from '../support/utils'
+
 const providersUrl = 'https://app.papernest.com/onboarding?anonymous&anonymousId=test&id_text=1&destination=newspaper'
-const subscriberPathname = '/mon-compte/presse/2'
+const referencePathname = '/mon-compte/presse/2'
 
 const providersSelector = '*[id^="newspaper-address_change.provider-"]'
-const subscriberNumberSelector = '*[id^="newspaper-address_change.reference"]'
+const referenceSelector = '*[id^="newspaper-address_change.reference"]'
+
 
 describe('Flow: Newspaper Address Change', { defaultCommandTimeout: 10000 }, () => {
   beforeEach(() => {
     Cypress.Cookies.preserveOnce('jwt')
-    })
+  })
 
+  describe('Newspaper page', () => {
     it('displays newspaper providers', () => {
+      cy.visit(providersUrl)
       cy.get(providersSelector)
         .should('have.length.greaterThan', 1)
     })
 
     it("redirects to Numero d'abonné page on provider click", () => {
-      cy.get(providersSelector)
-        .first()
-        .click()
-      cy.location('pathname').should('eq', subscriberPathname)
+      cy.get(providersSelector).first().click()
+      cy.location('pathname').should('eq', referencePathname)
+    })
+  })
+
+  describe("Numero d'abonné page", () => {
+    it('accepts a subscriber number and redirects to ', () => { 
+      cy.get(referenceSelector).type(randomAlphaNumeric(5))
     })
   })
 })
