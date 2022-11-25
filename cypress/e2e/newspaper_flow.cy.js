dfeb9b2d8b94f7aab08109a41da4c552bd43de71
Seb <sebastien.corrigan@gmail.com>
2022-11-25 18:27:09 +0100

    test: address page

--- cypress/e2e/newspaper_flow.cy.js
+++ cypress/e2e/newspaper_flow.cy.js
@@ -6,18 +6,23 @@ import { randomAlphaNumeric } from "../support/utils";
 const providersUrl =
   "https://app.papernest.com/onboarding?anonymous&anonymousId=test&id_text=1&destination=newspaper";
 const referencePathname = "/mon-compte/presse/2";
-const addressPathname = "/mon-compte/presse/3"
+const addressPathname = "/mon-compte/presse/3";
+const userInfoPathname = "/mon-compte/presse/4";
 
 // Selectors
-const providersSelector = '*[id^="newspaper-address_change.provider-"]';
-const referenceSelector = '*[id^="newspaper-address_change.reference"]';
+const providersSel = '*[id^="newspaper-address_change.provider-"]';
+const referenceSel = '*[id^="newspaper-address_change.reference"]';
+const buttonNextSel = '#button_next';
+const housingAddressSel = '*[id^="housing.address"]'
+const addressDropdownSel = '*[class^="dropdown-suggestions ng-star-inserted"]'
 
-// Storage variables for the purpose of confirmation testing
+// User variables for the purpose of confirmation testing
+const userAddress = '157 Boulevard Macdonald 75019 Paris';
 let selectedProviderName;
 
 describe(
   "Flow: Newspaper Address Change",
-  { defaultCommandTimeout: 10000 },
+  { defaultCommandTimeout: 5000 },
   () => {
     beforeEach(() => {
       Cypress.Cookies.preserveOnce("jwt");
@@ -28,16 +33,17 @@ describe(
         Cypress.runner.stop()
       }
     })
+
     describe("Newspaper page", () => {
       it("displays newspaper providers", () => {
         cy.visit(providersUrl);
         cy.wait(1000);
-        cy.get(providersSelector).should("have.length.greaterThan", 1);
+        cy.get(providersSel).should("have.length.greaterThan", 1);
       });
 
       it("redirects to Numero d'abonné page on provider click", () => {
         // Select a random newspaper provider
-        const selectedProvider = cy.get(providersSelector)
+        const selectedProvider = cy.get(providersSel)
           .then(($providers) => {
             return cy.get(Cypress._.sample($providers));
           });
@@ -54,13 +60,28 @@ describe(
 
     describe("Numero d'abonné page", () => {
       it("accepts a subscriber number and redirects to address page", () => {
-        cy.get('#button_next').should('be.visible').and('have.class', 'disabled')
+        cy.get(buttonNextSel).should('be.visible').and('have.class', 'disabled')
 
-        cy.get(referenceSelector).type(randomAlphaNumeric(5));
+        cy.get(referenceSel).type(randomAlphaNumeric(5));
 
-        cy.get('#button_next').should('be.visible').and('not.have.class', 'disabled').click()
+        cy.get(buttonNextSel).should('be.visible').and('not.have.class', 'disabled').click()
         cy.location("pathname").should("eq", addressPathname)
       });
     });
+
+    describe("Address page", () => {
+      it("accepts an address", () => {
+        cy.get(buttonNextSel).should('be.visible').and('have.class', 'disabled')
+
+        cy.get(housingAddressSel).should('not.have.class', 'checked').type(userAddress)
+
+        cy.get(addressDropdownSel).contains(userAddress).click()
+
+        cy.get(housingAddressSel).should('have.class', 'checked')
+
+        cy.get(buttonNextSel).should('be.visible').and('not.have.class', 'disabled').click()
+        cy.location("pathname").should("eq", userInfoPathname)
+      })
+    })
   }
 );
