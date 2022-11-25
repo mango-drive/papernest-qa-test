de32c481c2305a317a688083c10b0b35711a235f
Seb <sebastien.corrigan@gmail.com>
2022-11-25 17:54:49 +0100

    test: numéro d'abonné page

--- cypress/e2e/newspaper_flow.cy.js
+++ cypress/e2e/newspaper_flow.cy.js
@@ -1,12 +1,18 @@
 import { randomAlphaNumeric } from "../support/utils";
 
+
+
+// Urls and pathnames
 const providersUrl =
   "https://app.papernest.com/onboarding?anonymous&anonymousId=test&id_text=1&destination=newspaper";
 const referencePathname = "/mon-compte/presse/2";
+const addressPathname = "/mon-compte/presse/3"
 
+// Selectors
 const providersSelector = '*[id^="newspaper-address_change.provider-"]';
 const referenceSelector = '*[id^="newspaper-address_change.reference"]';
 
+// Storage variables for the purpose of confirmation testing
 let selectedProviderName;
 
 describe(
@@ -37,15 +43,18 @@ describe(
         });
 
         selectedProvider.click();
-
         cy.location("pathname").should("eq", referencePathname);
       });
     });
 
     describe("Numero d'abonné page", () => {
-      it("accepts a subscriber number and redirects to ", () => {
+      it("accepts a subscriber number and redirects to address page", () => {
+        cy.get('#button_next').should('be.visible').and('have.class', 'disabled')
+
         cy.get(referenceSelector).type(randomAlphaNumeric(5));
-        cy.log(selectedProviderName)
+
+        cy.get('#button_next').should('be.visible').and('not.have.class', 'disabled').click()
+        cy.location("pathname").should("eq", addressPathname)
       });
     });
   }
